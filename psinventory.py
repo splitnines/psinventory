31c73e9d9ffda3424461e02b2e9fe4e39ce25544
Rickey Ingrassia <splitnines@gmail.com>
2026-05-15 07:38:30 -0700

    put the import statement in alphabetical order

--- psinventory.py
+++ psinventory.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
-from typing import Any
-from pathlib import Path
 from datetime import datetime as dt
-import json
+from pathlib import Path
+from typing import Any
 import argparse
 import csv
+import json
 import sys
 
 
